7bfaa80e8ea1c923aba887ea0b934c11a4550632
quinlan-alys <shannonqad@gmail.com>
2017-01-04 13:55:40 -0500

    making fun jquery transitions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function () {
 
 $('.logo').hover(function() {
+    // $('.home-sect').animate({display:'none'}, 1000);
+    $('.home-sect').css('display','none');
+
     $('.menu ul li').animate({height: '20px'}, 1000)
+}) 
+$('.contact-li').click(function() {
+    console.log('hope');
+    $('.contact-sect').animate({height: '1000px'}, 1000)
 }) 
 
 });
