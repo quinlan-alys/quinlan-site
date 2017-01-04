7d0ce072060b75fdc4398fa72f141084b7d3ca3f
quinlan-alys <shannonqad@gmail.com>
2017-01-04 14:23:38 -0500

    adjusting for mobile first design

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
 
-$('.logo').hover(function() {
+$('.logo').click(function() {
     // $('.home-sect').animate({display:'none'}, 1000);
     $('.home-sect').css('display','none');
-
     $('.menu ul li').animate({height: '20px'}, 1000)
+    $('.contact-sect').css('display', 'none');
 }) 
+
+
 $('.contact-li').click(function() {
     console.log('hope');
+     $('.contact-sect').css('display', 'block');
     $('.contact-sect').animate({height: '1000px'}, 1000)
+    $('.menu ul li').animate({height: '0px'}, 1000)
 }) 
 
 });
